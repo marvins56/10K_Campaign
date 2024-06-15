7a277e293e6f12e433fea215d6d497ab36462efe
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2024-06-15 22:40:38 +0300

    feat: Add Fundraising entities and enums
    This commit adds the Fundraising entities and enums to the FSH.Starter project. The entities "Account", "Campaign", "CampaignStudent", "Configurations", "Donation", "DonationStudent", and "Fundraiser" have been included. The enum "CampaignStatus" has also been added. These changes are necessary to implement the core functionality of the fundraising module.
    
    Note: Please remove this note before committing the changes.

---
+++ FSH.Starter/src/Core/Domain/Campaign/Entities/Donation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSH.Starter.Domain.Fundraising.Entities;
+public class Donation
+{
+    public Guid DonationId { get; set; }
+    public Guid CampaignId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime DonationDate { get; set; }
+    public Guid DonorId { get; set; }
+    public Guid? ConfigurationsId { get; set; }
+
+    public Campaign Campaign { get; set; }
+    public Fundraiser Donor { get; set; }
+    public Configurations Configurations { get; set; }
+    public ICollection<DonationStudent> DonationStudents { get; set; }
+}
