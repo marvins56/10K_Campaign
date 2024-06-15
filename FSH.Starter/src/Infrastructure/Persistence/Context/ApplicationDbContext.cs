5d4528c33987e590391268ec3ac64e0775a32d19
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2024-06-15 17:47:23 +0300

    feat: Add CampaignStatus enum
    This commit adds the CampaignStatus enum to the FSH.Starter project. The enum includes three values: Active, Completed, and Cancelled. This enum will be used to track the status of fundraising campaigns.
    
    Note: Please remove this note before committing the changes.

--- FSH.Starter/src/Infrastructure/Persistence/Context/ApplicationDbContext.cs
+++ FSH.Starter/src/Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -2,6 +2,7 @@ using Finbuckle.MultiTenant;
 using FSH.Starter.Application.Common.Events;
 using FSH.Starter.Application.Common.Interfaces;
 using FSH.Starter.Domain.Catalog;
+using FSH.Starter.Domain.Fundraising.Entities;
 using FSH.Starter.Infrastructure.Persistence.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
@@ -17,11 +18,40 @@ public class ApplicationDbContext : BaseDbContext
 
     public DbSet<Product> Products => Set<Product>();
     public DbSet<Brand> Brands => Set<Brand>();
-
+    public DbSet<Campaign> Campaigns { get; set; }
+    public DbSet<Donation> Donations { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<CampaignStudent> CampaignStudents { get; set; }
+    public DbSet<DonationStudent> DonationStudents { get; set; }
+    public DbSet<Fundraiser> Fundraisers { get; set; }
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Configurations> Configurations { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.HasDefaultSchema(SchemaNames.Catalog);
+
+        modelBuilder.Entity<CampaignStudent>()
+            .HasKey(cs => new { cs.CampaignId, cs.StudentId });
+        modelBuilder.Entity<CampaignStudent>()
+            .HasOne(cs => cs.Campaign)
+            .WithMany(c => c.CampaignStudents)
+            .HasForeignKey(cs => cs.CampaignId);
+        modelBuilder.Entity<CampaignStudent>()
+            .HasOne(cs => cs.Student)
+            .WithMany(s => s.CampaignStudents)
+            .HasForeignKey(cs => cs.StudentId);
+
+        modelBuilder.Entity<DonationStudent>()
+            .HasKey(ds => new { ds.DonationId, ds.StudentId });
+        modelBuilder.Entity<DonationStudent>()
+            .HasOne(ds => ds.Donation)
+            .WithMany(d => d.DonationStudents)
+            .HasForeignKey(ds => ds.DonationId);
+        modelBuilder.Entity<DonationStudent>()
+            .HasOne(ds => ds.Student)
+            .WithMany(s => s.DonationStudents)
+            .HasForeignKey(ds => ds.StudentId);
     }
 }
