d2f0867323decf5ca5d7e37de23d1863798869ec
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2024-06-16 16:12:51 +0300

    feat: Add Microsoft.EntityFrameworkCore package reference and IApplicationDbContext interface
    This commit adds the Microsoft.EntityFrameworkCore package reference to the Application.csproj file, enabling the use of Entity Framework Core in the FSH.Starter project. Additionally, it introduces the IApplicationDbContext interface in the Common/Interfaces folder, which defines the necessary DbSet properties for the Fundraising entities. These changes are essential for implementing database operations and organizing the code related to the Fundraising module.

---
+++ FSH.Starter/src/Core/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using FSH.Starter.Domain.Fundraising.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSH.Starter.Application.Common.Interfaces;
+public interface IApplicationDbContext
+{
+   
+    DbSet<Campaign> Campaigns { get; set; }
+    DbSet<Donation> Donations { get; set; }
+    DbSet<Student> Students { get; set; }
+    DbSet<CampaignStudent> CampaignStudents { get; set; }
+    DbSet<DonationStudent> DonationStudents { get; set; }
+    DbSet<Fundraiser> Fundraisers { get; set; }
+    DbSet<Account> Accounts { get; set; }
+    DbSet<Configurations> Configurations { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
