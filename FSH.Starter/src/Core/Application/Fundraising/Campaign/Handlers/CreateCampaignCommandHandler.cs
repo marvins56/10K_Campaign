e729eda31e5799f03f961e9aee1097345bf6fac0
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2024-06-16 16:14:48 +0300

    feat: Add CreateCampaignCommandHandler, GetAllCampaignsQueryHandler, GetCampaignQueryHandler, and UpdateCampaignCommandHandler
    These changes add the necessary command and query handlers for the Campaign module in the FSH.Starter project. The handlers include CreateCampaignCommandHandler, GetAllCampaignsQueryHandler, GetCampaignQueryHandler, and UpdateCampaignCommandHandler. These handlers are essential for implementing the core functionality of the Campaign module and organizing the code related to it.

---
+++ FSH.Starter/src/Core/Application/Fundraising/Campaign/Handlers/CreateCampaignCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using FSH.Starter.Application.Fundraising.Campaign.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FSH.Starter.Domain.Fundraising.Entities;
+
+namespace FSH.Starter.Application.Fundraising.Campaign.Handlers;
+public class CreateCampaignCommandHandler : IRequestHandler<CreateCampaignCommand, Guid>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateCampaignCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Guid> Handle(CreateCampaignCommand request, CancellationToken cancellationToken)
+    {
+        var campaign = new FSH.Starter.Domain.Fundraising.Entities.Campaign
+        {
+            CampaignName = request.CampaignName,
+            Description = request.Description,
+            StartDate = request.StartDate,
+            EndDate = request.EndDate
+        };
+
+        _context.Campaigns.Add(campaign);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return campaign.CampaignId;
+    }
+}
---
+++ FSH.Starter/src/Core/Application/Fundraising/Campaign/Handlers/UpdateCampaignCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using FSH.Starter.Application.Fundraising.Campaign.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSH.Starter.Application.Fundraising.Campaign.Handlers;
+public class UpdateCampaignCommandHandler : IRequestHandler<UpdateCampaignCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public UpdateCampaignCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(UpdateCampaignCommand request, CancellationToken cancellationToken)
+    {
+        var campaign = await _context.Campaigns.FindAsync(new object[] { request.CampaignId }, cancellationToken);
+
+        if (campaign == null)
+        {
+            //throw an exception if the campaign is not found
+
+            throw new NotFoundException(nameof(Campaign));
+        }
+
+        campaign.CampaignName = request.CampaignName;
+        campaign.Description = request.Description;
+        campaign.StartDate = request.StartDate;
+        campaign.EndDate = request.EndDate;
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
