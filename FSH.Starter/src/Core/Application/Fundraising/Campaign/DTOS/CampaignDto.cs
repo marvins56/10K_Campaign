8347b39b8d30e223e4dcb9116d6f7e59ec2e7a0a
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2024-06-16 16:13:49 +0300

    feat: Add Campaign module DTOs and commands
    This commit adds the necessary DTOs and commands for the Campaign module in the FSH.Starter project. The files "CampaignDto.cs", "CreateCampaignCommand.cs", "CreateCampaignRequest.cs", "GetAllCampaignsQuery.cs", "GetCampaignQuery.cs", "UpdateCampaignCommand.cs", and "UpdateCampaignRequest.cs" have been included. These changes are essential for implementing the core functionality of the Campaign module and organizing the code related to it.

---
+++ FSH.Starter/src/Core/Application/Fundraising/Campaign/DTOS/CampaignDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSH.Starter.Application.Fundraising.Campaign.DTOS;
+public class CampaignDto
+{
+    public DefaultIdType CampaignId { get; set; }
+    public string CampaignName { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Description { get; set; }
+}
---
+++ FSH.Starter/src/Core/Application/Fundraising/Campaign/DTOS/UpdateCampaignRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace FSH.Starter.Application.Fundraising.Campaign.DTOS;
+public class UpdateCampaignRequest
+{
+    public Guid CampaignId { get; set; }
+    public string CampaignName { get; set; }
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
