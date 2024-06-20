5f2f1f0821632b87ae6d3f4dc0da2b324286daca
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2024-06-20 14:16:57 +0300

    feat: Add FundraiserProfile and MappingProfile classes
    These code changes add the FundraiserProfile and MappingProfile classes to the FSH.Starter project. The FundraiserProfile class is responsible for mapping the Account entity to the AccountDto class, while the MappingProfile class maps the Fundraiser entity to the FundraiserDto class. These changes are essential for implementing object mapping in the Campaign module and organizing the code related to it.

---
+++ FSH.Starter/src/Core/Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FSH.Starter.Application.Fundraising.Campaign.DTOS;
+using FSH.Starter.Domain.Fundraising.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FSH.Starter.Application.Common.Mappings;
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Fundraiser, FundraiserDto>()
+            .ForMember(dest => dest.AccountName, opt => opt.MapFrom(src => src.Account.AccountName));
+    }
+}
