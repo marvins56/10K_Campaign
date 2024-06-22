39dcb9df1d2794d59139f0b1dcf1e85227f95f79
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2024-06-23 02:34:27 +0300

    feat: Add custom exceptions for create and update failures
    These code changes add custom exception classes for create and update failures in the Common.Exceptions namespace of the FSH.Starter project. The CreateFailureException and UpdateFailureException classes are used to handle specific scenarios where the creation or update of an entity fails. These exceptions provide more detailed information about the failure, improving error handling and debugging capabilities.

---
+++ FSH.Starter/src/Core/Application/Common/Exceptions/CreateFailureException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace FSH.Starter.Application.Common.Exceptions;
+[Serializable]
+internal class CreateFailureException : Exception
+{
+    public CreateFailureException()
+    {
+    }
+
+    public CreateFailureException(string? message) : base(message)
+    {
+    }
+
+    public CreateFailureException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected CreateFailureException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
---
+++ FSH.Starter/src/Core/Application/Common/Exceptions/UpdateFailureException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace FSH.Starter.Application.Common.Exceptions;
+[Serializable]
+internal class UpdateFailureException : Exception
+{
+    public UpdateFailureException()
+    {
+    }
+
+    public UpdateFailureException(string? message) : base(message)
+    {
+    }
+
+    public UpdateFailureException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected UpdateFailureException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
