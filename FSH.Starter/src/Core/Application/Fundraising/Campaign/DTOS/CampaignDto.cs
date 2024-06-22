6c9de70b8fdfc6bb5d8660b8c658565a8b4bfcc7
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2024-06-23 02:37:29 +0300

    feat: Add Fundraisers property to AccountDto and additional properties to CampaignDto
    These code changes add the Fundraisers property to the AccountDto class in the FSH.Starter project. This property is a list of FundraiserDto objects and enhances the functionality of the AccountDto.
    
    Additionally, the CampaignDto class is updated with the TargetAmount property, as well as the Donations, CampaignStudents, and Configurations properties. These changes improve the data structure and capabilities of the CampaignDto.

--- FSH.Starter/src/Core/Application/Fundraising/Campaign/DTOS/CampaignDto.cs
+++ FSH.Starter/src/Core/Application/Fundraising/Campaign/DTOS/CampaignDto.cs
@@ -5,11 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace FSH.Starter.Application.Fundraising.Campaign.DTOS;
+//public class CampaignDto
+//{
+//    public DefaultIdType CampaignId { get; set; }
+//    public string CampaignName { get; set; }
+//    public DateTime StartDate { get; set; }
+//    public DateTime EndDate { get; set; }
+//    public string Description { get; set; }
+//}
+
 public class CampaignDto
 {
-    public DefaultIdType CampaignId { get; set; }
+    public Guid CampaignId { get; set; }
     public string CampaignName { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public string Description { get; set; }
+    public decimal TargetAmount { get; set; }
+    public List<DonationDto> Donations { get; set; }
+    public List<CampaignStudentDto> CampaignStudents { get; set; }
+    public List<ConfigurationDto> Configurations { get; set; }
 }
---
+++ FSH.Starter/src/Core/Application/Fundraising/Campaign/DTOS/ConfigurationDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSH.Starter.Application.Fundraising.Campaign.DTOS;
+public class ConfigurationDto
+{
+    public Guid ConfigurationsId { get; set; }
+    public string ConfigType { get; set; }
+    public string ConfigValue { get; set; }
+    public Guid? CampaignId { get; set; }
+    public Guid? DonationId { get; set; }
+    public string Description { get; set; }
+    public bool IsActive { get; set; }
+    public decimal? MinDonationAmount { get; set; }
+    public decimal? MaxDonationAmount { get; set; }
+    public int? MinNumberOfStudents { get; set; }
+    public int? MaxNumberOfStudents { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public string? AccountingCode { get; set; }
+}
