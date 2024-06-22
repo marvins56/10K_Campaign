dee9ca7e17c389e426012594bf0bf42f09fd55a5
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2024-06-22 22:53:28 +0300

    Custom exceptions added

--- FSH.Starter/src/Core/Application/Fundraising/Campaign/Handlers/CreateDonationStudentCommandHandler.cs
+++ FSH.Starter/src/Core/Application/Fundraising/Campaign/Handlers/CreateDonationStudentCommandHandler.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static FSH.Starter.Application.Common.Exceptions.NotFoundException;
 
 namespace FSH.Starter.Application.Fundraising.Campaign.Handlers;
 public class CreateDonationStudentCommandHandler : IRequestHandler<CreateDonationStudentCommand>
@@ -19,31 +20,47 @@ public class CreateDonationStudentCommandHandler : IRequestHandler<CreateDonatio
 
     public async Task<Unit> Handle(CreateDonationStudentCommand request, CancellationToken cancellationToken)
     {
-        var donation = await _context.Donations
-            .FirstOrDefaultAsync(d => d.DonationId == request.DonationId, cancellationToken);
 
-        if (donation == null)
+        try
         {
-            throw new NotFoundException(nameof(Donation));
+            var donation = await _context.Donations
+           .FirstOrDefaultAsync(d => d.DonationId == request.DonationId, cancellationToken);
+
+            if (donation == null)
+            {
+                throw new NotFoundException(nameof(Donation));
+            }
+
+            var student = await _context.Students
+                .FirstOrDefaultAsync(s => s.StudentId == request.StudentId, cancellationToken);
+
+            if (student == null)
+            {
+                throw new NotFoundException(nameof(Student));
+            }
+            //check if that student has already been assigned to that donation
+            var donationStudentExists = await _context.DonationStudents
+                .AnyAsync(ds => ds.DonationId == request.DonationId && ds.StudentId == request.StudentId, cancellationToken);
+            if (donationStudentExists)
+            {
+                throw new ResultExistsException(nameof(Student));
+            }
+
+
+            var donationStudent = new DonationStudent
+            {
+                DonationId = request.DonationId,
+                StudentId = request.StudentId
+            };
+
+            _context.DonationStudents.Add(donationStudent);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
         }
-
-        var student = await _context.Students
-            .FirstOrDefaultAsync(s => s.StudentId == request.StudentId, cancellationToken);
-
-        if (student == null)
+        catch (Exception ex)
         {
-            throw new NotFoundException(nameof(Student));
+            throw new Exception(ex.Message);
         }
-
-        var donationStudent = new DonationStudent
-        {
-            DonationId = request.DonationId,
-            StudentId = request.StudentId
-        };
-
-        _context.DonationStudents.Add(donationStudent);
-        await _context.SaveChangesAsync(cancellationToken);
-
-        return Unit.Value;
     }
 }
